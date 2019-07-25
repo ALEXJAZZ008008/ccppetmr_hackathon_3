ecb4f1365d21d01467faab53d8ad862e78910646
HTunne <h.tunnicliffe@protonmail.com>
2019-07-25 18:24:43 +0100

    Begin work on transform_data function.

--- generate_data.py
+++ generate_data.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import sirf.STIR as pet
@@ -8,7 +9,24 @@ from generate_image import generate_image
 
 def transform_image(fixed_image):
 
-    return np.array((1,2,3)), fixed_image
+    moving_image = fixed_image.clone()
+
+    resampler = Reg.NiftyResample()
+
+    transform = Reg.AffineTransformation(np.array([[1,0,0,0],
+                                                   [0,1,0,0],
+                                                   [0,0,1,0],
+                                                   [0,0,0,1]]))
+
+    resampler.add_transformation(transform)
+
+    resampler.set_reference_image(fixed_image)
+    resampler.set_floating_image(moving_image)
+    resampler.set_interpolation_type(3)
+
+    resampler.process()
+
+    return np.array((1,2,3)), moving_image
 
 
 def generate_data(initial_image, num_images, num_tranforms):
@@ -19,15 +37,18 @@ def generate_data(initial_image, num_images, num_tranforms):
         fixed_image = generate_image(initial_image)
 
         fixed_image.write('training_data/fixed/fixed_{:03d}'.format(i))
+        try:
+            os.mkdir('training_data/moving/fixed_{:03d}'.format(i))
+        except:
+            pass
 
         for j in range(num_tranforms):
             transform, moving_image = transform_image(fixed_image)
 
             transforms[ ((i+1) * (j+1)) - 1 ] = transform
 
-
             moving_image.write(
-                'training_data/moving/moving_{:03d}_fixed_{:03d}'.format(j, i))
+                'training_data/moving/fixed_{:03d}/moving_{:03d}'.format(i, j))
     np.savetxt('training_data/transforms.csv', transforms, delimiter=',')
 
 def main():
@@ -39,3 +60,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
