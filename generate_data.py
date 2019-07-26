c6b5ce19b2f31042874342276f06e707a08c6bbc
HTunne <h.tunnicliffe@protonmail.com>
2019-07-26 13:58:37 +0100

    Add transformations to data generation.

--- generate_data.py
+++ generate_data.py
@@ -1,4 +1,7 @@
+import subprocess
 import os
+import math
+import random
 import numpy as np
 import matplotlib.pyplot as plt
 import sirf.STIR as pet
@@ -7,54 +10,89 @@ import sirf.Reg as Reg
 from generate_image import generate_image
 
 
-def transform_image(fixed_image):
+def transform_image(fixed_im_name, moving_im_name):
 
-    moving_image = fixed_image.clone()
+    trans_file = 'temp_trans_file.txt'
 
-    resampler = Reg.NiftyResample()
+    angle = random.uniform(-1,1)
+    tr_x = random.uniform(-1,1)
+    tr_y = random.uniform(-1,1)
 
-    transform = Reg.AffineTransformation(np.array([[1,0,0,0],
-                                                   [0,1,0,0],
-                                                   [0,0,1,0],
-                                                   [0,0,0,1]]))
+    theta = angle * (math.pi / 4)
 
-    resampler.add_transformation(transform)
+    #print('angle: {}\ntr_x: {}\ntr_y: {}\ntheta: {}'.format(angle, tr_x, tr_y,
+    #                                                        theta))
 
-    resampler.set_reference_image(fixed_image)
-    resampler.set_floating_image(moving_image)
-    resampler.set_interpolation_type(3)
+    transform = Reg.AffineTransformation(np.array(
+        [[math.cos(theta),-math.sin(theta),0,tr_x*25],
+         [math.sin(theta),math.cos(theta),0,tr_y*25],
+         [0,0,1,0],
+         [0,0,0,1]]))
 
-    resampler.process()
+    transform.write(trans_file)
 
-    return np.array((1,2,3)), moving_image
+#    args = ["reg_resample", "-ref", "training_data/fixed/fixed_000.nii",
+#            "-flo", "training_data/fixed/fixed_000.nii", "-res", "test",
+#            "-trans", "temp_trans_file.txt"]
+#
+#    popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+#    popen.wait()
+#
+
+    args = ["reg_resample", "-ref", fixed_im_name + '.nii', "-flo",
+            fixed_im_name + '.nii', "-res", moving_im_name + '.nii', "-trans",
+            trans_file]
+    popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+    popen.wait()
+
+    os.remove(trans_file)
+
+    return [tr_x, tr_y, angle]
 
 
 def generate_data(initial_image, num_images, num_tranforms):
+    """
+    Generate data for image registration NN.
 
+    initial_image   =   Initial image used to generate fixed and moving images.
+    num_images      =   Number of fixed images to generate.
+    num_tranforms   =   number and transforms (and miving images) to generate
+                        for each fixed image.
+    """
     transforms = np.zeros((num_images * num_tranforms, 3))
 
-    for i in range(num_images):
-        fixed_image = generate_image(initial_image)
+    with open('training_data/transforms.csv', 'w') as transform_csv:
 
-        fixed_image.write('training_data/fixed/fixed_{:03d}'.format(i))
-        try:
-            os.mkdir('training_data/moving/fixed_{:03d}'.format(i))
-        except:
-            pass
+        for i in range(num_images):
+            fixed_image = generate_image(initial_image)
 
-        for j in range(num_tranforms):
-            transform, moving_image = transform_image(fixed_image)
+            fixed_im_name = 'training_data/fixed/fixed_{:03d}'.format(i)
 
-            transforms[ ((i+1) * (j+1)) - 1 ] = transform
+            fixed_image.write(fixed_im_name)
 
-            moving_image.write(
-                'training_data/moving/fixed_{:03d}/moving_{:03d}'.format(i, j))
-    np.savetxt('training_data/transforms.csv', transforms, delimiter=',')
+            args = ("stir_math", "--output-format", "stir_ITK.par", fixed_im_name,
+                 fixed_im_name + '.hv')
+            popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+            popen.wait()
+
+            try:
+                os.mkdir('training_data/moving/fixed_{:03d}'.format(i))
+            except:
+                pass
+
+            for j in range(num_tranforms):
+
+                moving_im_name = 'training_data/moving/fixed_{:03d}/moving_{:03d}'.format(i, j)
+
+                transform = transform_image(fixed_im_name, moving_im_name)
+
+                transform_csv.write(','.join(str(tr) for tr in transform) +
+                                    '\n')
+
 
 def main():
-    initial_image = pet.ImageData('blank_image.hv')
 
-    print(initial_image.as_array().shape)
+    initial_image = pet.ImageData('blank_image.hv')
 
     generate_data(initial_image, 10, 10)
 
--- generate_image.py
+++ generate_image.py
@@ -1,9 +1,13 @@
 import matplotlib.pyplot as plt
 import sirf.STIR as pet
+import sirf.Reg as Reg
 import random
 
 
 def generate_image(initial_image):
+    """
+    Add a random number of ellipsis to an  initial image.
+    """
 
     image_shape = initial_image.as_array().shape
 
@@ -13,20 +17,23 @@ def generate_image(initial_image):
         shape = pet.EllipticCylinder()
 
         shape.set_length(1)
-        shape.set_radii((random.uniform(1, image_shape[1]/4),
-                         random.uniform(1, image_shape[2]/4)))
+        shape.set_radii((random.uniform(1, image_shape[1]/8),
+                         random.uniform(1, image_shape[2]/8)))
 
-        shape.set_origin((random.uniform(-(image_shape[0]/2), image_shape[0]/2),
-                          random.uniform(-(image_shape[1]/2), image_shape[1]/2),
-                          random.uniform(-(image_shape[2]/2),
-                                         image_shape[2]/2)))
+        radii = shape.get_radii()
+
+        shape.set_origin((0,
+                          random.uniform(-(image_shape[1]/4) + radii[1],
+                                         image_shape[1]/4 - radii[1]),
+                          random.uniform(-(image_shape[2]/4) + radii[0],
+                                         image_shape[2]/4 - radii[0])))
 
         image.add_shape(shape, scale = random.uniform(0,1))
 
     return image
 
 if __name__ == "__main__":
-    initial_image = pet.ImageData('blank_image.hv')
+    initial_image = Reg.ImageData('blank_image.hv')
     print(initial_image.as_array().shape)
     image = generate_image(initial_image)
 
